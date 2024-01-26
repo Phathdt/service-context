b244d936a7d84aaf27ef6732329c68697b2a8fb0
Phathdt <phathdt379@gmail.com>
2024-01-26 17:03:57 +0700

    Revert "add debug"
    This reverts commit 1b1e5449ec452b856db25beee20373cd51465f3a.

--- component/mongoc/mongoc.go
+++ component/mongoc/mongoc.go
@@ -3,7 +3,6 @@ package mongoc
 import (
 	"context"
 	"flag"
-	"fmt"
 
 	sctx "github.com/phathdt/service-context"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -41,11 +40,7 @@ func (m *mongoClient) Activate(sc sctx.ServiceContext) error {
 
 	clientOptions := options.Client().ApplyURI(m.mongoUri)
 
-	fmt.Println("mongo >>>>>>>>>>>>>>>>>>>>>>>>>")
-	fmt.Printf("mongo enable_tracing = %t", sctx.EnableTracing)
-	fmt.Println("mongo >>>>>>>>>>>>>>>>>>>>>>>>>")
 	if sctx.EnableTracing {
-		fmt.Println("im in mongo")
 		clientOptions.Monitor = mongotrace.NewMonitor()
 	}
 
