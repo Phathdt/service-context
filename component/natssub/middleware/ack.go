03442cf1b9cb9e3f126d4745477a84b4e789a647
Phathdt <phathdt379@gmail.com>
2024-03-29 11:52:10 +0700

    rename middlware ack

--- component/natssub/middleware/ack.go
+++ component/natssub/middleware/ack.go
@@ -2,7 +2,7 @@ package middleware
 
 import "github.com/nats-io/nats.go/jetstream"
 
-func AckMiddleware() MiddlewareFunc {
+func Ack() MiddlewareFunc {
 	return func(handler func(msg jetstream.Msg)) func(msg jetstream.Msg) {
 		return func(msg jetstream.Msg) {
 			defer msg.Ack()
