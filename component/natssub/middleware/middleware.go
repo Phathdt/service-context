1b513062f607475130dcda2606c12a462d6c8c1b
Phathdt <phathdt379@gmail.com>
2024-03-29 11:50:47 +0700

    rename middleware

--- component/natssub/middleware/middleware.go
+++ component/natssub/middleware/middleware.go
@@ -6,7 +6,7 @@ import (
 
 type MiddlewareFunc func(handler func(msg jetstream.Msg)) func(msg jetstream.Msg)
 
-func ApplyMiddleware(handler func(msg jetstream.Msg), middlewares ...MiddlewareFunc) func(msg jetstream.Msg) {
+func Apply(handler func(msg jetstream.Msg), middlewares ...MiddlewareFunc) func(msg jetstream.Msg) {
 	for _, middleware := range middlewares {
 		handler = middleware(handler)
 	}
